808a39f1ca4a14aa7f0e32c01fb09f66a0541ad9
Glitch <siliconrob@siliconheaven.net>
2018-01-12 21:33:16 +0000

    🍑🌱 Checkpoint server.js:4054/765 package.json:4054/1

--- server.js
+++ server.js
@@ -10,55 +10,21 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.post("/locationPower", function (request, response) { 
-  
-  console.log(request.body);  
-  
+app.post("/locationPower", function (req, res) {   
+  console.log(request.body);    
   const options = {
     url: 'http://nominatim.openstreetmap.org/search',
     method: 'GET',
-    headers: headers,
-    qs: { q: request.body.text, format: 'json', limit: 1 }
+    qs: { q: req.body.text, format: 'json', limit: 1 }
   };
   
   // Start the request
   request(options, function (error, res, body) {
     if (!error && res.statusCode == 200) {
         // Print out the response body
-        console.log(body)
-    };
-  
-  const position = {
-    lat: 32, //request.body.lat,    
-    lng: -97 //request.body.lng    
-  };
-  
-  request.
-  
-  
-  console.log(`Power location received: (${position.lat}, ${position.lng})`);  
-  const point = solcast.latLng(position.lat, position.lng);
-  const options = solcast.Options.power();
-  options.APIKey = process.env.SOLCAST_API_KEY;
-  options.Capacity = request.body.capacity || 1000;
-  
-  const results = solcast.Power.forecast(point, options);
-  results.then(results => {
-    
-    var now = new Date;
-    var utc_timestamp = Date.UTC(now.getUTCFullYear(),now.getUTCMonth(), now.getUTCDate(), now.getUTCHours() + 4, now.getUTCMinutes(), now.getUTCSeconds(), now.getUTCMilliseconds());
-    var filtered_results = results.forecasts.filter(z => {
-      var current = new Date(z.period_end);
-      if (current < utc_timestamp) {
-        return current;
-      }
-    }).map(k => {
-      const timestamp = k.period_end.replace('T',' ').split('.')[0]+" UTC";      
-      return `${timestamp}: ${k.pv_estimate}`;
-    });
-    response.send(filtered_results.join('\n'));
-  })
-  .catch(err => { console.log(err); });  
+        console.log(body);
+        res.send("hello");
+    }});
 });
 
 const listener = app.listen(process.env.PORT, function () {
