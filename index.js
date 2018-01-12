3b572d9336f58ecef08382427ede63286b0d7359
Glitch <siliconrob@siliconheaven.net>
2018-01-12 23:08:27 +0000

    🍷🚣 Checkpoint index.html:4054/43 index.js:4054/73

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const request = require('request');
 const bodyParser = require('body-parser');
 const rp = require('request-promise');
 const solcast = require('solcast');
@@ -11,6 +12,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/slack', function(req, res){
+  if (!req.query.code) { // access denied
+    res.redirect('https://solcast-slack.glitch.me/');
+    return;
+  }
+  var data = {form: {
+      client_id: process.env.SLACK_CLIENT_ID,
+      client_secret: process.env.SLACK_CLIENT_SECRET,
+      code: req.query.code
+  }};
+  request.post('https://slack.com/api/oauth.access', data, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // Get an auth token
+      let token = JSON.parse(body).access_token;
+
+      // Get the team domain name to redirect to the team URL after auth
+      request.post('https://slack.com/api/team.info', {form: {token: token}}, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          if(JSON.parse(body).error == 'missing_scope') {
+            res.send('Solcast Power Forecast has been added to your team!');
+          } else {
+            let team = JSON.parse(body).team.domain;
+            res.redirect('http://' +team+ '.slack.com');
+          }
+        }
+      });
+    }
+  })
 });
 
 app.post("/locationPower", function (request, response) {   
