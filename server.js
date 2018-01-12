38fee6d965f8362c2e30b3a48cc55e24dfc849d4
Glitch <siliconrob@siliconheaven.net>
2018-01-12 18:35:56 +0000

    🗃🐷 Checkpoint server.js:4054/157

--- server.js
+++ server.js
@@ -10,6 +10,23 @@ app.get("/", function (request, response) {
 
 app.post("/locationDetails", function (request, response) {  
   const position = {
+    lat: request.body.lat,    
+    lng: request.body.lng,
+  } 
+  console.log(`Location received: (${position.lat}, ${position.lng})`);  
+  const point = solcast.latLng(position.lat, position.lng);
+  const options = solcast.Options.power();
+  radiationOptions.APIKey = process.env.SOLCAST_API_KEY;
+  
+  const results = solcast.Radiation.forecast(point, radiationOptions);
+  results.then(results => {    
+    response.send(results.forecasts);
+  })
+  .catch(err => { console.log(err); });  
+}
+
+app.post("/locationDetails", function (request, response) {  
+  const position = {
     lat: request.query.lat,    
     lng: request.query.lng,
   } 
